537181512db98ef83e345dbdbd9e384e7b7f7aef
Roberto Barbosa <betoanselmo@gmail.com>
2024-02-16 11:42:34 -0300

    Solve Tests

---
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,52 @@
+package com.devsuperior.bds04.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repositories.CityRepository;
+
+@Service
+public class CityService {
+	
+	@Autowired
+	private CityRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll(){
+		List<City> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return  new CityDTO(entity);
+	}
+
+	public void delete(Long id) {
+
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);		
+		}
+		
+	}
+}
